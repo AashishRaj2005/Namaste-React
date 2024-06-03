739ec44be90007995727756841b86b0464d08559
Aashish Raj <rajaashish2005@gmail.com>
2024-06-04 01:35:54 +0530

    ok

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm an h1 tag"),
@@ -5,8 +8,8 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
 
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
+    React.createElement("h1", {}, "An Tag"),
+    React.createElement("h2", {}, "Parcel you are slow"),
   ]),
 ]);
 
