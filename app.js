7869d6cc963d0345ce4d4174fdba4ffb75676593
Aashish Raj <rajaashish2005@gmail.com>
2024-06-05 02:51:42 +0530

    msg

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "This is a heading"
-);
+// jsx code is also there
+const heading = <h1 id="heading">Hello world from jsx</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
