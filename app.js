7f4f9c5b9bd87cc8c757e6feadb990b84fe9e9c2
Aashish Raj <rajaashish2005@gmail.com>
2024-06-05 13:05:07 +0530

    JSX to React element to html

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ import ReactDOM from "react-dom/client";
 // jsx code is also there
 const heading = <h1 id="heading">Hello world from jsx</h1>;
 
+//todo react component can be written in many ways
+
+const Heading = () => {
+  return (
+    <h1 id="heading" className="H1">
+      Hello react functional component
+    </h1>
+  );
+};
+const H = () => true;
+
+const h = () => <h1 className="baskar">Yeyyyy</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
-
