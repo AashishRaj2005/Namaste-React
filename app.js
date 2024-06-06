4b225780370927d55556ac1fef147d4629695f9d
Aashish Raj <rajaashish2005@gmail.com>
2024-06-06 17:01:08 +0530

    kardo

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Img } from "react-image";
+
 // jsx code is also there
 const heading = <h1 id="head">Hello world from jsx</h1>;
 
 // **todo
-
 
 /*make a header
 -logo
@@ -15,9 +16,31 @@ body
 
 footer
 
-*/ 
+*/
 
+const Header = () => (
+  <div className="header">
+    <div className="logo">
+      <img src="https://img.freepik.com/premium-vector/good-food-logo-template_79169-17.jpg?w=740" />
+    </div>
+    <div className="navItems">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  </div>
+);
 
+const Applayout = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Heading />);
+root.render(<Applayout />);
