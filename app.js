189c9cdcf3b9836c8de0d21ac9d5cb0cc57a1590
Aashish Raj <rajaashish2005@gmail.com>
2024-06-06 14:58:59 +0530

    kardo

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ import ReactDOM from "react-dom/client";
 // jsx code is also there
 const heading = <h1 id="head">Hello world from jsx</h1>;
 
-//todo react component can be written in many ways
+// **todo
 
-const Heading = () => {
-  return (
-    <div>
-      {heading}
-      <h1 id="heading">Hello react functional component</h1>
-      {console.log("mission Success")}
-    </div>
-  );
-};
+
+/*make a header
+-logo
+-searchbar
+-profile
+
+body
+
+footer
+
+*/ 
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(<Heading />);
