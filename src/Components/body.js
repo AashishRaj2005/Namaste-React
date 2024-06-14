7dd746e5ede7b0dcb04d3074ed7672a59c573855
Aashish Raj <rajaashish2005@gmail.com>
2024-06-14 21:55:51 +0530

    arwdhjbgfjkdfbkj

--- src/Components/body.js
+++ src/Components/body.js
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import resList from "../Utils/mockdata";
 import RestaurantCard from "./Card";
 
 const Body = () => {
   const [ListofRest, setListofRes] = useState(resList);
+
+  useEffect(() => {
+    fetchData;
+  }, []);
+
+  const fetchData = async () => {
+    const Data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+  
+  const json = await Data.json();
+  console.log(json);
+
+  setListofRes(data?.cards?.1?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+  };
   return (
     <div className="body">
       <div className="searchBar">
@@ -16,7 +32,9 @@ const Body = () => {
             );
             setListofRes(filteredList);
           }}
-        >Filter Res</button>
+        >
+          Filter Res
+        </button>
       </div>
 
       <div className="res-container">
